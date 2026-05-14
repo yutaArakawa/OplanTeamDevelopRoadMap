ac4159c60938b080873d2a87b1204624e4711986
Yuta Arakawa <y.arakawa71@icloud.com>
2026-05-14 17:58:55 +0900

    簡易ログインページ実装

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name='accounts/login.html'
+        ),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout'
+    ),
+]
--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGIN_REDIRECT_URL = '/accounts/login/'
