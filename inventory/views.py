f9f0fed3e1e35395630a412f66a7e7993b9e1550
Ito Jun <j.ito@oplan.co.jp>
2026-05-18 16:42:06 +0900

    商品管理画面の作成

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from common.mixins import AdminRequiredMixin
+from inventory.models import Goods, GoodsCategory
+
+
+class GoodsListView(AdminRequiredMixin, TemplateView):
+    template_name = 'inventory/goods_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        goods = Goods.objects.select_related('goods_category').all()
+        category_id = self.request.GET.get('category')
+
+        if category_id:
+            goods = goods.filter(goods_category_id=category_id)
+
+        context['goods_list'] = goods.order_by('goods_name')
+        context['categories'] = GoodsCategory.objects.all().order_by('category_name')
+        context['selected_category'] = category_id or ''
+        return context
+
+
+class GoodsCreateView(AdminRequiredMixin, TemplateView):
+    template_name = 'inventory/goods_create.html'
