8c3ff2a6071f1027e4dfbe53d4f063525d527d08
Yuta Arakawa <y.arakawa71@icloud.com>
2026-06-11 15:40:33 +0900

    各ディレクトリ内のstaticをプロジェクト直下のstaticに移動しnginx+Gunicorn構成でも即時css/jsの修正が反映されるよう修正

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_REDIRECT_URL = '/'
 
