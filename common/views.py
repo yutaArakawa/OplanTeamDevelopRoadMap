9c1d06e20c2512f410bcef7fee926429cb60633e
Ito Jun <j.ito@oplan.co.jp>
2026-06-15 18:00:40 +0900

    検索窓に候補が出ない不具合の対応。絞り込みボタンの統一。発注履歴がない場合、htmlでボタン押下可否の制御をおこないましt。

--- common/views.py
+++ common/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from common.seializers import ShopNameSerializer, WarehouseNameSerializer
 
 from inventory.models import (
@@ -11,14 +11,18 @@ from inventory.models import (
 # Create your views here.
 
 
-class ShopNameAutoCompleteView(LoginRequiredMixin, APIView):
+class ShopNameAutoCompleteView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         query = request.GET.get('q', '')
         shops = Shop.active_objects.filter(shop_name__icontains=query)[:10]
         serializer = ShopNameSerializer(shops, many=True)
         return Response(serializer.data)
 
-class WarehouseNameAutoCompleteView(LoginRequiredMixin, APIView):
+class WarehouseNameAutoCompleteView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         query = request.GET.get('q', '')
         warehouses = Warehouse.active_objects.filter(warehouse_name__icontains=query)[:10]
