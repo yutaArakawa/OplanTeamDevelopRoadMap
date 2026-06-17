356f19140057ebb36ed199932f52df393d07f05d
Yuta Arakawa <y.arakawa71@icloud.com>
2026-06-17 16:34:54 +0900

    開発環境のキャッシュ不使用対応＆本番環境キャッシュ使用・キャッシュバスティング対応改修

--- nginx/script/entrypoint.sh
+++ nginx/script/entrypoint.sh
@@ -1,2 +1,2 @@
 # envsubst：テンプレート（nginx.conf）の $BASIC を環境変数の値に置換して本番用に保存し、Nginxをフォアグラウンドで起動
-envsubst '$BASIC' < /etc/nginx/temp/nginx.conf > /etc/nginx/conf.d/nginx.conf && nginx -g 'daemon off;'
+envsubst '$BASIC $STATIC_CACHE_CONTROL $STATIC_DIR' < /etc/nginx/temp/nginx.conf > /etc/nginx/conf.d/nginx.conf && nginx -g 'daemon off;'
