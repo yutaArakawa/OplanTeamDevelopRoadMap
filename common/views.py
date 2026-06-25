99b1c2c88fd2793d0625e7be03bbb0e780d895b1
Ito Jun <j.ito@oplan.co.jp>
2026-06-25 09:51:48 +0900

    Mixinの変更を戻しました。

--- common/views.py
+++ common/views.py
@@ -1,12 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from common.seializers import ShopNameSerializer, WarehouseNameSerializer
 from inventory.constants.prefectures import PREFECTURE_CHOICES
 
 from inventory.models import (Shop, Warehouse)
+# Create your views here.
 
 
-class ShopNameAutoCompleteView(APIView):
+class ShopNameAutoCompleteView(LoginRequiredMixin, APIView):
     def get(self, request):
         query = request.GET.get('q', '')
         shops = Shop.active_objects.filter(shop_name__icontains=query)[:10]
@@ -14,15 +18,15 @@ class ShopNameAutoCompleteView(APIView):
         return Response(serializer.data)
 
 
-class WarehouseNameAutoCompleteView(APIView):
+class WarehouseNameAutoCompleteView(LoginRequiredMixin, APIView):
     def get(self, request):
         query = request.GET.get('q', '')
         warehouses = Warehouse.active_objects.filter(warehouse_name__icontains=query)[:10]
         serializer = WarehouseNameSerializer(warehouses, many=True)
         return Response(serializer.data)
+    
 
-
-class PrefectureNameAutoCompleteView(APIView):
+class PrefectureNameAutoCompleteView(LoginRequiredMixin, APIView):
     def get(self, request):
         query = request.GET.get('q', '')
         prefectures = [
